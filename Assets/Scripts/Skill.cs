53de16d6c25f097ab5e6f164b42772792768c1cd
yuzukosho <a.i.rhango@gmail.com>
2017-09-16 01:19:37 +0900

    [追加] Skill関係のコンポーネント追加
    * SkillManager:
        スキルを管理するコンポーネント
        スキルON/OFF，MPの増減，スキルの入れ替えを実装
    * Skill:
        スキルの抽象class
        このclassを継承してスキルを作る

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Skill : MonoBehaviour {
+	public float cost;
+
+	protected virtual void Awake () {
+		enabled = false;
+	}
+
+	protected abstract void OnEnable ();
+	protected abstract void OnDisable ();
+}
---
+++ Assets/Scripts/SkillManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillManager : MonoBehaviour {
+	public float maxMagicPoint = 100.0f;
+	public float mugicPoint = 100.0f;
+	public float recoveryAmount = 1.0f;
+
+	Skill[] skillSlots = new Skill[3];
+	bool isActive = false;
+
+	void Start () {
+		// 空Skillをセット
+	}
+
+	void FixedUpdate () {
+		if (isActive) {
+			foreach (Skill skill in skillSlots) {
+				mugicPoint -= skill.cost;
+			}
+			if (mugicPoint <= 0) {
+				Deactivate();
+				mugicPoint = 0;
+			}
+		} else {
+			if (mugicPoint < maxMagicPoint) {
+				mugicPoint += recoveryAmount;
+				if (mugicPoint > maxMagicPoint) {
+					mugicPoint = maxMagicPoint;
+				}
+			}
+		}
+	}
+
+	public void ReplaceSkill (int num, string name) {
+		System.Type skillType = System.Type.GetType(name);
+		Destroy(skillSlots[num]);
+		skillSlots[num] = gameObject.AddComponent(skillType) as Skill;
+		skillSlots[num].enabled = isActive;
+	}
+
+	public void ReplaceSkills (string[] names) {
+		for (int i = 0; i < 3; i++) {
+			ReplaceSkill(i, names[i]);
+		}
+	}
+
+	public void Activate () {
+		isActive = true;
+		foreach (Skill skill in skillSlots) {
+			skill.enabled = true;
+		}
+	}
+
+	public void Deactivate () {
+		isActive = false;
+		foreach (Skill skill in skillSlots) {
+			skill.enabled = false;
+		}
+	}
+}
