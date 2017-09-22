6456456fef069d6d3fe7d357c99358bf5c3feb03
rhango <a.i.rhango@gmail.com>
2017-09-23 01:27:15 +0900

    [更新] Damageに属性をつけられるようにした
    * Damageに即死攻撃などの属性をつけられるようにした
    * 属性はenumで定義し，それぞれのDamageにHashSetとして付加する

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum DAMAGE_ATTRIBUTE {
+    INSTANT_DEATH,
+    CAPTURE
+}
+
 public class Damage : MonoBehaviour {
     public float attackPoint;
+    public HashSet<DAMAGE_ATTRIBUTE> attribute = new HashSet<DAMAGE_ATTRIBUTE>();
 }
